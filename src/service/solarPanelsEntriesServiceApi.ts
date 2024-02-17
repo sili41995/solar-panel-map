aac7e5fe646874249f78e2f61d2317cf97c78694
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-02-17 19:19:13 +0200

    update service

--- src/service/solarPanelsEntriesServiceApi.ts
+++ src/service/solarPanelsEntriesServiceApi.ts
@@ -1,9 +1,11 @@
 import { ITargetEntry, SolarPanelEntries } from '@/types/types';
 
 class SolarPanelsEntriesServiceApi {
+  private BASE_URL = 'https://api3.geo.admin.ch/rest/services';
+
   getAllEntries(): Promise<{ results: SolarPanelEntries }> {
     return fetch(
-      'https://api3.geo.admin.ch/rest/services/api/MapServer/identify?geometryType=esriGeometryPoint&geometry=600989,197423.7&tolerance=400&imageDisplay=608,360,96&mapExtent=600913,197378.7,601065,197468.7'
+      `${this.BASE_URL}/api/MapServer/identify?geometryType=esriGeometryPoint&geometry=600989,197423.7&tolerance=400&imageDisplay=608,360,96&mapExtent=600913,197378.7,601065,197468.7`
     ).then((response) => {
       if (!response.ok) {
         throw new Error('Not Found');
@@ -15,7 +17,7 @@ class SolarPanelsEntriesServiceApi {
 
   getEntryById(id: string): Promise<{ feature: ITargetEntry }> {
     return fetch(
-      `https://api3.geo.admin.ch/rest/services/all/MapServer/ch.bfe.solarenergie-eignung-daecher/${id}?geometryFormat=esriGeojson`
+      `${this.BASE_URL}/all/MapServer/ch.bfe.solarenergie-eignung-daecher/${id}?geometryFormat=esriGeojson`
     ).then((response) => {
       if (!response.ok) {
         throw new Error('Not Found');
