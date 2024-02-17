367175282704d8955ffed278f1e6b884f6e904e2
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-02-17 19:05:45 +0200

    add errorHandler

--- src/service/solarPanelsEntriesServiceApi.ts
+++ src/service/solarPanelsEntriesServiceApi.ts
@@ -4,13 +4,25 @@ class SolarPanelsEntriesServiceApi {
   getAllEntries(): Promise<{ results: SolarPanelEntries }> {
     return fetch(
       'https://api3.geo.admin.ch/rest/services/api/MapServer/identify?geometryType=esriGeometryPoint&geometry=600989,197423.7&tolerance=400&imageDisplay=608,360,96&mapExtent=600913,197378.7,601065,197468.7'
-    ).then((response) => response.json());
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error('Not Found');
+      }
+
+      return response.json();
+    });
   }
 
   getEntryById(id: string): Promise<{ feature: ITargetEntry }> {
     return fetch(
       `https://api3.geo.admin.ch/rest/services/all/MapServer/ch.bfe.solarenergie-eignung-daecher/${id}?geometryFormat=esriGeojson`
-    ).then((response) => response.json());
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error('Not Found');
+      }
+
+      return response.json();
+    });
   }
 }
 
