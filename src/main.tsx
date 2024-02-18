c2f0042b37b3c9af7c507fab4756f693fa8a5711
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-02-18 18:29:54 +0200

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '@/components/App';
 import { BrowserRouter } from 'react-router-dom';
+import * as React from 'react';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
