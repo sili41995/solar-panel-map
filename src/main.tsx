56bd48ff327f88a9dc68da85b392493480546541
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-02-18 18:19:48 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <ThemeProvider theme={theme}> */}
       <App />
-      {/* <GlobalStyles /> */}
-      {/* </ThemeProvider> */}
     </BrowserRouter>
   </React.StrictMode>
 );
